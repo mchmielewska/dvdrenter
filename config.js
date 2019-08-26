543a2aadc160a4dad481a23330bcbe32943e637c
Monika Chmielewska <moniika.chmielewska@gmail.com>
2019-08-26 18:37:17 +0200

    Dotenv

--- config.js
+++ config.js
@@ -1,15 +1,4 @@
-const dotenv = require('dotenv');
-const result = dotenv.config();
-console.log(result);
-
 module.exports = {
     port: process.env.port,
    dbc: process.env.dbconnection
 }
-//if(result.error){
-//   throw result.error;
-//}
-
-//const {parsed: env} = result;
-
-//module.exports = env;
